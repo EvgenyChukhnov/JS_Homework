41498f3e7d7a79a7baf19145589ac13fd83f7ca2
evgenych@gmail.com <evgenych@gmail.com>
2020-11-30 12:41:29 +0300

    init: homework 10 deepClone

---
+++ 10/10_Object_deepClone.js
@@ -0,0 +1,40 @@
+var initialObj = {
+  string: 'Vasya',
+  number: 30,
+  boolean: true,
+  undefined: undefined,
+  null: null,
+  array: [1, 2, 3],
+  object: {
+      string2: 'Petrov',
+      object2: {
+          array2: [{}, {}]
+      },
+      object3: {}
+  },
+  method: function() {
+      alert('Hello');
+  }
+};
+
+console.log('initialObj:');
+console.log(initialObj);
+var clonedObj = deepClone(initialObj);
+
+clonedObj.object.object2.array2[1].name = 'Vasya';
+clonedObj.array.push(2);
+
+console.log(initialObj);
+console.log(clonedObj);
+
+function deepClone(obj) {
+  var objCloned = {};
+
+  for (var key in obj) {
+    if (obj[key] !== Object || !obj[key].isArray) {
+      objCloned[key] = obj[key];
+    } else objCloned[key] = cloneObj(obj[key]);
+  };
+
+  return objCloned;
+};
