93dd37068a116411fd32759caf5230557e33b991
evgenych@gmail.com <evgenych@gmail.com>
2020-12-03 01:45:02 +0300

    fix: array clone into array finally fix

--- 10/10_Object_deepClone.js
+++ 10/10_Object_deepClone.js
@@ -1,49 +1,63 @@
 var initialObj = {
-  string: 'Vasya',
-  number: 30,
-  boolean: true,
-  undefined: undefined,
-  null: null,
-  array: [1, 2, 3],
-  object: {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
       string2: 'Petrov',
       object2: {
-          array2: [{}, {}]
+        array2: [{}, {}]
       },
       object3: {}
-  },
-  method: function() {
-    console.log('Hello');
-  }
+    },
+method: function() {
+  console.log('Hello');
+}
 };
 
 var clonedObj = deepClone(initialObj);
 
 clonedObj.object.object2.array2[1].name = 'Vasya';
 clonedObj.array.push(2);
-clonedObj.string = 'Seryoja';
 
-console.log('initialObj:');
 console.log(initialObj);
-console.log('clonedObj:');
 console.log(clonedObj);
 
 
 function deepClone(obj) {
-  var objCloned = new Object;
+  var objCloned = {};
 
   for (var key in obj) {
     if (obj[key] === null) {
-        objCloned[key] = obj[key];
+      objCloned[key] = obj[key];
     } else if (Array.isArray(obj[key])) {
-      // var arr = [];
-      objCloned[key] = deepClone(obj[key]);
-
+      objCloned[key] = deepCloneArr(obj[key]);
     } else if (typeof obj[key] === 'object') { 
-        objCloned[key] = deepClone(obj[key]);
+      objCloned[key] = deepClone(obj[key]);
     } else {
       objCloned[key] = obj[key];
     }
   };
-    return objCloned;
+      return objCloned;
+
+function deepCloneArr(arr) {
+  var arrCloned = [];
+  
+  if (Array.isArray(arr)){
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === null) {
+        arrCloned[i] = arr[i];
+      } else if (Array.isArray(arr[i])) {
+        arrCloned[i] = deepCloneArr(arr[i]);
+      } else if (typeof arr[i] === 'object') { 
+        arrCloned[i] = deepClone(arr[i]);
+      } else {
+        arrCloned[i] = arr[i];
+      }
+    };
+  };
+      return arrCloned;
+}
 };
