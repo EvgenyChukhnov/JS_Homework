380dcf3e78ffc2667751f74d65fe5c98fd3f4cb5
evgenych@gmail.com <evgenych@gmail.com>
2020-12-08 19:01:53 +0300

    fix: minor fixes in methods

--- 11/Homework_11.js
+++ 11/Homework_11.js
@@ -2,10 +2,9 @@ console.log(filterNumbersArr([-1, 0, 2, 34, -2]));
 
 function filterNumbersArr(numbers) {
 
-   var newArr = numbers.filter(function(el) { 
+    return numbers.filter(function(el) { 
     return el > 0;
   });    
-    return newArr;
 };
 
 var arrNum = [-1, -3, -436, 4, -16, 26, 38, 456, 23, 11, -654, 876, 6, 789, 9, 2];
@@ -13,7 +12,9 @@ var arrNum = [-1, -3, -436, 4, -16, 26, 38, 456, 23, 11, -654, 876, 6, 789, 9, 2
 console.log(findPosNum(arrNum));
 
 function findPosNum(arr) {
-  return arr.find(function(item) { return item > 0 });
+  return arr.find(function(item) { 
+    return item > 0 
+  });
 };
 
 console.log(isPalindrome('шалаШ')); // true
@@ -22,8 +23,8 @@ console.log(isPalindrome('аРгентиНамаНИтНегра'));
 
 function isPalindrome(word) {
 
-  if (word.toLowerCase() === word.toLowerCase().split('').reverse().join('')) return true;
-    return false;
+  return word.toLowerCase() === word.toLowerCase().split('').reverse().join('');
+
 };
 
 
@@ -33,8 +34,8 @@ console.log(areAnagrams('кот', 'отко')); // false
 console.log(areAnagrams('asamala', 'amlasaa'));
 
 function areAnagrams(word1, word2) {
-  if (word1.toLowerCase().split('').sort().join('') === word2.toLowerCase().split('').sort().join('')) return true;
-  return false;
+  return word1.toLowerCase().split('').sort().join('') ===
+        word2.toLowerCase().split('').sort().join('');
 };
 
 console.log(divideArr([1, 2, 3, 4], 2)); // [[1, 2], [3, 4]]
