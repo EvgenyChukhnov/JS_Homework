7d7007f21b377a723451ce0d69d8a182d9e31bab
evgenych@gmail.com <evgenych@gmail.com>
2020-12-15 02:59:45 +0300

    feat: implement at all

--- 15/practical_task_13_upg.js
+++ 15/practical_task_13_upg.js
@@ -1,31 +1,52 @@
-var container = document.getElementById('container');
+var container = document.getElementById('container'),
+    firstPar = document.createElement('p'),
+    secondPar = document.createElement('p'),
+    linksFirstPar = firstPar.children,
+    button = document.getElementsByTagName('button')[0],
+    counter = 0;
 
-var firstPar = document.createElement('p'),
-    secondPar = document.createElement('p');
+  firstPar.innerHTML = 'Hello, here are <a href="https://www.facebook.com">Link 1</a> and <a href="https://twitter.com">Link 2</a>';
+  secondPar.innerHTML = 'Hello, here are <a href="http://google.by">Link 3</a> and <a href="https://vk.com">Link 4</a>';
 
-firstPar.innerHTML = 'Hello, here are <a href="https://www.facebook.com">Link 1</a> and <a href="https://twitter.com">Link 2</a>';
-secondPar.innerHTML = 'Hello, here are <a href="http://google.by">Link 3</a> and <a href="https://vk.com">Link 4</a>';
+  container.appendChild(firstPar);
+  container.appendChild(secondPar);
 
-container.appendChild(firstPar);
-container.appendChild(secondPar);
+  document.addEventListener("DOMContentLoaded", localStorageClear);
+  button.addEventListener('click', addClick);
 
-var button = document.getElementsByTagName('button')[0],
-    linksFirstPar = firstPar.children;
+  secondPar.onclick = function (event) {
+    var target = event.target,
+      pathFromLS = '';
 
-    secondPar.onclick = function (event) {
       event.preventDefault();
-      console.dir(event.target);
-      if (event.target.tagName == 'P' ) {
-        alert('you clicked on the second paragraph!')
-      } else { alert(event.target) };
+
+    if (target.tagName === 'A') {
+      if (!localStorage.getItem(target.text)) {
+        localStorage.setItem(target.text, JSON.stringify({path: target.origin}));
+        target.href = '#';
+        alert('link was saved!');
+        } else {
+            pathFromLS = JSON.parse(localStorage.getItem(target.text));
+            alert(pathFromLS.path) 
+        };
+    
     };
-
-button.addEventListener('click', addClick);
-
+  };
 
 function addClick () {
+  var colorArr = ['red', 'green', 'blue', 'yellow', 'purple'];
 
   for (var i = 0; i < linksFirstPar.length; i++){
-    linksFirstPar[i].classList.toggle('red');
-  }
+    var n = linksFirstPar[i].className;
+      if (n) {
+        linksFirstPar[i].classList.remove(n);
+      };
+    linksFirstPar[i].classList.add(colorArr[counter]);
+  };
+  counter++;
+  if(counter > 4) {counter = 0;};
+};
+
+function localStorageClear() {
+  localStorage.clear();
 };
