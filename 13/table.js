f8ec1bdda875bd7bfdb04756904a464af9deff92
evgenych@gmail.com <evgenych@gmail.com>
2020-12-14 04:46:33 +0300

    implement: events on focus & blur

--- 13/table.js
+++ 13/table.js
@@ -1,8 +1,39 @@
-var table = document.getElementsByTagName('table')[0],
-    addRowCell = table.getElementsByClassName('addRow')[0];
+var table = document.getElementsByTagName('table')[0];
+var text = '';
 
-    addRowCell.onclick = function(){
-      table.insertAdjacentHTML('afterbegin', '<tr><td></td><td></td><td></td></tr>');
+table.addEventListener('click', tableClick);
+table.addEventListener('blur', tableClickBlur, true);
+
+function tableClick(event) {
+  var target = event.target;
+  var myInput = document.createElement('input');
+
+  if (target.className === 'addRow' ) {
+    table.firstElementChild.insertAdjacentHTML('afterbegin', '<tr><td></td><td></td><td></td></tr>');
+    return;
+  };
+  
+  if (target.tagName === 'TD') {
+
+    if (target.textContent) {
+      text = target.textContent;
     };
+    target.textContent = '';
 
-  console.dir(addRowCell);
+    if (target.firstElementChild) {
+      target.firstElementChild.focus();
+    } else { 
+      target.appendChild(myInput).focus(); 
+      target.firstElementChild.value = text;
+    };                      
+    text = '';                             
+  };
+};
+
+function tableClickBlur (event) {
+  var target = event.target;
+  var tableCellBlur = target.parentElement;
+
+  tableCellBlur.textContent = target.value;
+  target.value = '';
+};
